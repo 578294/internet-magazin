865f3b7da2c5d12c853eeacb96ecae770dfa8204
578294 <5782_94@mail.ru>
2025-01-24 20:30:11 +0300

    Добавил класс Shop

--- main.py
+++ main.py
@@ -30,5 +30,29 @@ class Order:
         return sum(product.price for product in self.products)
 
     def __str__(self):
-        product_list = ", ".join(products)
+        products_list = ", ".join(self.products)
         return f"Order(Status: {self.status}, Total: {self.calculate_total()}, Products: {products_list}"
+
+class Shop:
+    def __init__(self):
+        self.products = []
+        self.orders = []
+        self.status = "Новый"
+
+    def add_product(self, product: Product):
+        self.products.append(product)
+
+    def remove_product(self, product_id):
+        self.products = [i for i in self.products if i.id != product_id]
+
+    def add_orders(self, orders: Order):
+        self.orders.append(orders)
+
+    def remove_orders(self, order_id):
+        self.orders = [i for i in self.orders if i.id != order_id]
+
+    def change_status(self, new_status):
+        self.status = new_status
+
+    def calculate_total(self):
+        return sum(product.price for order in self.orders)
